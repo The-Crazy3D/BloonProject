e6d2523d38bcac277584a577dd946bf2172922ae
PathFinder <tbriet@gmail.com>
2013-03-29 21:23:33 +0100

    Update config.php
    All comments has been translated in English

--- config.php
+++ config.php
@@ -2,19 +2,19 @@
 /*
  * BloonCrypto
  * Habbo R63 Post-Shuffle
- * By Burak (skype: burak.karamahmut)
+ * Based on the work of Burak, edited by BloonCrypto Git Community. (skype: burak.karamahmut)
  * 
  * https://github.com/BurakDev/BloonCrypto
  */
 $CONFIG = Array();
-$CONFIG['bindAddr'] = "0.0.0.0"; // IP du serveur (0.0.0.0 pour être accesible de partout)
-$CONFIG['bindPort'] = 99; // Port du serveur
-$CONFIG['ipaccess'] = true; // Configurer un accès par IP au serveur
-$CONFIG['ipaccesslist'] = Array("127.0.0.1"); // Liste des IP si ipaccess est activé (true)
+$CONFIG['bindAddr'] = "0.0.0.0"; // Server IP (you can use 0.0.0.0 for allow access from everywhere)
+$CONFIG['bindPort'] = 99; // Server Port
+$CONFIG['ipaccess'] = true; // Configure a access by ip to the server
+$CONFIG['ipaccesslist'] = Array("127.0.0.1"); // List of ip if ipaccess is enabled (true)
 $CONFIG['mysql'] = Array();
-$CONFIG['mysql']['host'] = "127.0.0.1";
-$CONFIG['mysql']['port'] = 3306;
-$CONFIG['mysql']['user'] = "root";
-$CONFIG['mysql']['database'] = "database";
-$CONFIG['mysql']['password'] = "password";
+$CONFIG['mysql']['host'] = "127.0.0.1"; // SQL Host IP (no hostname)
+$CONFIG['mysql']['port'] = 3306; // SQL Host Port (MySQL use 3306 by default)
+$CONFIG['mysql']['user'] = "root"; // SQL User Name (root by default)
+$CONFIG['mysql']['database'] = "database"; // SQL Database
+$CONFIG['mysql']['password'] = "password"; // SQL Password
 ?>
