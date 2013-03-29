38bef808502dac4b74a3da913db0d5a27783c650
BurakDev <burakadama@gmail.com>
2013-03-29 22:38:10 +0100

    Added release code on user login

---
+++ BloonCrypto/handler/handler_4000.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * BloonCrypto
+ * Habbo R63 Post-Shuffle
+ * Based on the work of Burak, edited by BloonCrypto Git Community. (skype: burak.karamahmut)
+ * 
+ * https://github.com/BurakDev/BloonCrypto
+ */
+$release = $core->GetNextString($data);
+$core->say("Loaded ".$release[0]);
+?>
