b970cdecb052da8a5160e5bda4af380e886bd3b7
PathFinder <tbriet@gmail.com>
2013-03-30 16:46:56 +0100

    Fix the updater.
    The updater has been fixed, he works.

--- class/class.updater.php
+++ class/class.updater.php
@@ -13,11 +13,12 @@ Class Updater{
 		$localbuild = file_get_contents("revision");
 		Console::WriteLine("Git build : ".$gitbuild.", Local build : ".$localbuild);
 		if($gitbuild == $localbuild){
-			Console::WriteLine("completed! No update needed.");
+			Console::WriteLine("Completed! No update needed.");
 		}else if($gitbuild > $localbuild){
-			Console::WriteLine("completed! BloonCrypto need update !");
+			Console::WriteLine("Completed! BloonCrypto need update !");
 			Console::WriteLine();
-			Console::WriteLine("If you want update emulator from github, say yes.");
+			Console::WriteLine("If you want update emulator from GitHub, say y (or yes)");
+			Console::WriteLine("The Update will be in the folder : BloonProject-BloonCrypto");
 			Console::Write(">>> ");
 			$get = trim(fgets(STDIN));
 			Switch($get){
@@ -48,46 +49,53 @@ Class Updater{
 		Console::WriteLine("Unzip finished. Now emulator close, restart it.");
 		exit;
 	}
-	public static function _unzip($zipfile, $dest){
-	if (extension_loaded('zip'))
-	{
-	  $zip = zip_open($zipfile);
-	  if (substr( $dest, strlen( $dest ) - 1 ) != '/')
-		$dest = $dest . '/';
-	  if ($zip)
-	  {
-		while ($zip_entry = zip_read($zip))
-		{
-		  $file = zip_entry_name($zip_entry);
-		  //print $file;
-		  $aFolderStructure = preg_split('@[\\|/]@', $file);
-		  if (count($aFolderStructure) > 1)
-		  {
-			$f = '';
-			for ($i = 0; $i < count($aFolderStructure) - 1; $i++)
-			{
-			  if (!file_exists($dest . $f . $aFolderStructure[$i]) && !is_dir($dest . $f . $aFolderStructure[$i]))
-			  {
-				mkdir($dest . $f . $aFolderStructure[$i], 0666);
-				$f = $f . $aFolderStructure[$i] . '/';
-			  }
-			}
-		  }
-		  //$file = zip_entry_name($zip_entry);
-		  $fp = @fopen($dest . $file, "w+");
-		  if (zip_entry_open($zip, $zip_entry, "r"))
-		  {
-			$buf = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
-			zip_entry_close($zip_entry);
-		  }
-		  fwrite($fp, $buf);
-		  fclose($fp);
-		}
-		zip_close($zip);
-		return true;
-	  }
-	  return false;
-	}
-	}
+	
+
+function _unzip($zipfile, $dest)
+{
+if (extension_loaded('zip')){
+$zip = zip_open($zipfile);	
+if (substr( $dest, strlen( $dest ) - 1 ) != '/')
+$dest = $dest . '/';
+
+if ($zip){
+while ($zip_entry = zip_read($zip))
+{
+$file = zip_entry_name($zip_entry);
+$aFolderStructure = preg_split('@[\\|/]@', $file);
+
+if (count($aFolderStructure) > 1){
+for ($i = 0; $i < count($aFolderStructure) - 1; $i++){
+$dossierFull = $dest;
+for ($j = 0; $j < count($aFolderStructure) - 1; $j++){
+if (substr( $dossierFull, strlen( $dossierFull ) - 1 ) != '/')
+$dossierFull = $dossierFull . '/';
+
+$dossierFull = $dossierFull . $aFolderStructure[$j];
+}
+
+if (!file_exists($dossierFull) && !is_dir($dossierFull))
+mkdir($dossierFull, 0666);
+}
+}
+
+if(!is_dir($dest . $file)){
+$fp = @fopen($dest . $file, "w+");
+
+if(zip_entry_open($zip, $zip_entry, "r")){
+$buf = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+zip_entry_close($zip_entry);
+}
+
+fwrite($fp, $buf);
+fclose($fp);
+}
+}
+zip_close($zip);
+return true;
+}
+return false;
+}
+}
 }
 ?>
