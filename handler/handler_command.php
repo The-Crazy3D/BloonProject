db42b305e95fa4249404ebd76d0dd75672b4ac5d
Burak <burakadama@gmail.com>
2013-04-07 16:42:18 +0200

    Added :sit command

--- handler/handler_command.php
+++ handler/handler_command.php
@@ -77,6 +77,19 @@ if($smessage[0] == ":"){
 				$send = true;
 			}
 		break;
+		case ":sit":
+			$construct = New Constructor;
+			$construct->SetHeader(Packet::GetHeader('UpdateState'));
+			$construct->SetInt24(1);
+			$construct->SetInt24($user->userid);
+			$construct->SetInt24($user->pos_x);
+			$construct->SetInt24($user->pos_y);
+			$construct->SetStr($user->pos_z,true);
+			$construct->SetInt24($user->rotate);
+			$construct->SetInt24($user->rotate);
+			$construct->SetStr("/flatcrtl 4 useradmin/sit ".($user->pos_z +0.55)."//",true);
+			Core::SendToAllRoom($user->room_id, $construct->get());
+		break;
 		case ":shutdown":
 			if(Core::PermissionRank($user->rank, "cmd_shutdown")){
 				Core::Cmdlogs($user->userid,$user->username, "shutdown", $message, time());
